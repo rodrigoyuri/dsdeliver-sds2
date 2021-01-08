3c2e2a6192915f7d54c51042585e8b8a96b05914
Rodrigo Yuri <rodrigo33996@gmail.com>
2021-01-08 17:26:53 -0300

    correct name class css

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -9,10 +9,10 @@ function Home() {
             <div className="home-container">
                 <div className="home-content">
                     <div className="home-actions">
-                        <h1 className="home title">
+                        <h1 className="home-title">
                             Faça seu pedido <br /> que entregamos <br /> pra você!!!
                         </h1>
-                        <h3 className="home subtitle">
+                        <h3 className="home-subtitle">
                             Escolha o seu pedido e em poucos minutos <br />
                             levaremos na sua porta
                         </h3>
